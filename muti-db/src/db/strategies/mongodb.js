aaaaaeda2f4e4d607f887aa5149dc0481de8973f
Murillo Welsi <murillo.welsi@gmail.com>
2021-01-13 20:03:26 +0000

    add strategy pattern

---
+++ muti-db/src/db/strategies/mongodb.js
@@ -0,0 +1,13 @@
+const ICrud = require('./interfaces/interfaceCrud')
+
+class MongoDb extends ICrud {
+  constructor() {
+    super()
+  }
+
+  create(item) {
+    console.log('O item foi salvo no MongoDb')
+  }
+}
+
+module.exports = MongoDb
